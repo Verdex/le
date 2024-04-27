9889f66ef78f5b285b16a363dcc2e563bc2c4754
David L. Inman <davidlevere@gmail.com>
2024-04-27 19:05:45 -0400

    inc prog for number lexer, etc

--- src/parsing/lexer.rs
+++ src/parsing/lexer.rs
@@ -8,7 +8,7 @@ pub enum LexError {
 
 #[derive(Debug)]
 pub enum Token {
-
+    Number(Box<str>),
 }
 
 pub fn lex(input : &mut I) -> Result<Vec<Token>, LexError> {
@@ -19,9 +19,12 @@ pub fn lex(input : &mut I) -> Result<Vec<Token>, LexError> {
         Idle,
     }
 
+    let mut ts : Vec<Token> = vec![];
+
+    let mut left_over : Option<(usize, char)> = None;
     let mut state = State::Idle;
     loop {
-        match input.next() {
+        match left_over.or_else(|| input.next()) {
             Some((_, '/')) if state == State::StartSlash => { 
                 state = State::Idle;
                 line_comment(input)?; 
@@ -31,13 +34,27 @@ pub fn lex(input : &mut I) -> Result<Vec<Token>, LexError> {
                 state = State::Idle;
                 block_comment(input)?;
             },
-            //Some((_, x)) if x.is_digit() => ,
+            Some((_, x)) if x.is_digit(10) || x == '-' => {
+                state = State::Idle;
+                let n = number(x, input)?;
+            },
             //Some((_, x)) if x.is_digit() => ,
             _ => todo!(),
         }
     }
+
+    Ok(ts)
 }
 
+fn number(init : char, input : &mut I) -> Result<(Option<(usize, char)>, Token), LexError> {
+    todo!()
+    /*let xs = vec![];
+    loop {
+        match input.next() {
+
+        }
+    }*/
+}
 
 fn line_comment(input : &mut I) -> Result<(), LexError> {
     loop {
@@ -64,8 +81,8 @@ fn block_comment(input : &mut I) -> Result<(), LexError> {
     loop {
         match input.next() {
             Some((_, '*')) if state == State::StartSlash => {  
-                state == State::Idle;
-                next_level += 1;
+                state = State::Idle;
+                nest_level += 1;
             },
 
             Some((_, '*')) => { state = State::EndStar; },
