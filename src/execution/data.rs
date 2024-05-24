f3ef564d99e3a2f1bd08d084345d2690ab50b0c9
David L. Inman <davidlevere@gmail.com>
2024-05-23 21:40:24 -0400

    inc prog: instr

--- src/execution/data.rs
+++ src/execution/data.rs
@@ -1,6 +1,16 @@
 
 pub enum Instr {
-
+    Goto(usize),
+    Ret,
+    Call(usize),
+    // TODO the below instrs need some thought
+    PushParam(usize),
+    PushStack(VmData),
+    SetStack(usize, VmData),
+    GetStack(usize),
+    SetMemory(usize, VmData),
+    GetMemory(usize),
+    PushMemory(VmData),
 }
 
 pub enum Ir {
