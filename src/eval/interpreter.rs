55bf94b63aafc3b518145e500724f772d78183ef
David L. Inman <davidlevere@gmail.com>
2024-08-05 21:32:11 -0400

    TODO

--- src/eval/interpreter.rs
+++ src/eval/interpreter.rs
@@ -3,6 +3,8 @@
 pub struct Interpreter {
 }
 
+// TODO:  le patterns probably need:  generators, foreach, seq, and anon-structs (or row poly or pattern env types)
+
 impl Interpreter {
     pub fn new() -> Self { 
         Interpreter { }
