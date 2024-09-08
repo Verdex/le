fd36967d4e9138f9509d0c3e2b87b6609752b441
David L. Inman <davidlevere@gmail.com>
2024-09-07 20:53:18 -0400

    inc prog for linearizer

--- src/typing/linearizer.rs
+++ src/typing/linearizer.rs
@@ -1,10 +1,9 @@
 
-/*
 use std::collections::HashSet;
-use crate::data::{Ast, Linear, Type, Val, Sym, FunAddr, Stmt};
+use crate::data::{Ast, vm};
 
 
-pub fn linearize(ast : Vec<Ast>) -> Vec<Linear> {
+pub fn linearize(ast : Vec<Ast>) -> Vec<vm::Fun> {
     ast.into_iter().map(to_linear).collect()
 }
 
@@ -12,15 +11,13 @@ pub fn linearize(ast : Vec<Ast>) -> Vec<Linear> {
 // the intepreter won't accept a top level Expr because
 // a REPL will want to parse Expr and then wrap in an
 // artifical main function.
-fn to_linear(ast : Ast) -> Linear {
+fn to_linear(ast : Ast) -> vm::Fun {
     match ast {
         Ast::Number(s) => todo!(),
         Ast::Variable(sym) => todo!(),
         Ast::Call { fun_expr, inputs } => {
             // TODO : env will need to be pre-existing items for repl runs
-            let (a, mut c) = call(*fun_expr, *inputs, HashSet::new());
-            c.push(Stmt::ReturnAnonVar(a));
-            repl_main_fun_wrapper(c)
+            todo!()
         },
 
         // name : symbol
@@ -32,22 +29,8 @@ fn to_linear(ast : Ast) -> Linear {
     }
 }
 
-// func_expr : expr
-// inputs : syntax list of expr
-fn call(fun_expr : Ast, inputs : Ast, env : HashSet<Box<str>>) -> (usize, Vec<Stmt>) {
-   /* match fun_expr {
-        Ast::Call { fun_expr, inputs } => call(*fun_expr, *inputs, env),
-        Ast::Variable(sym) => {
-            match *sym {
-                Ast::Symbol(s) if env.contains(&s) => { },
-                x => { panic!("variable should contain symbol, but found {:?}", x); },
-            }
-        },
-        _ => todo!(),
-    }*/
-    todo!()
-}
 
+/*
 fn repl_main_fun_wrapper(body : Vec<Stmt>) -> Linear {
     Linear::Fun { name : gen_sym("repl_main_wrapper")
                 , params : vec![] 
@@ -55,16 +38,11 @@ fn repl_main_fun_wrapper(body : Vec<Stmt>) -> Linear {
                 , body 
                 }
 }
+*/
 
 fn anon() -> usize {
     static mut index : usize = 0;
     unsafe { let v = index; index += 1; v }
-}
-
-fn gen_sym(name : &str) -> Sym {
-    static mut index : usize = 0;
-    let v = unsafe { let v = index; index += 1; v };
-    Sym::Gen(name.into(), v)
 }
 
 #[cfg(test)]
@@ -72,4 +50,3 @@ mod test {
     use super::*;
 
 }
-*/
