665e4b11083a053c88f9dc7d257c6771fe20b09e
David L. Inman <davidlevere@gmail.com>
2025-05-23 20:24:28 -0400

    jlnexus provides other ways to create a parser

--- src/parsing/lexer.rs
+++ src/parsing/lexer.rs
@@ -68,8 +68,7 @@ impl From<Vec<LexError>> for LexError {
 impl std::error::Error for LexError { }
 
 pub fn lex(input : Rc<str>) -> Result<Vec<Token>, LexError> {
-    let input = input.chars().collect::<Vec<_>>();
-    let mut buffer = Parser::new(&input);
+    let mut buffer = input.chars().collect::<Parser<char>>();
 
     let mut tokens = vec![];
 
